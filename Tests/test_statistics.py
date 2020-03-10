51261799f000006f549c267bc582b19bb91ff102
anushajawed <aj588@njit.edu>
2020-03-10 14:46:16 -0400

    task: add mean

---
+++ Tests/test_statistics.py
@@ -0,0 +1,28 @@
+import unittest
+from random import random
+
+import unittest
+from Statistics.Statistics import Statistics
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.statistics = Statistics()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.statistics, Statistics)
+
+    def test_decorator_calculator(self):
+        self.assertIsInstance(self.statistics, Statistics)
+
+# Test Mean -------------------------------------------------------------
+    def test_statistics_calculator_return_mean(self):
+        data = [1,2,3,4,5]
+        result = self.statistics.mean(data)
+        self.assertEqual(3, result)
+
+ # Test Mean -------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    unittest.main()
