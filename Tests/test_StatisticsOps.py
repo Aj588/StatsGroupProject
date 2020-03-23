e60e313df7ac32bf75d11749120e60c1988fba37
po47 <po47@njit.edu>
2020-03-23 15:15:27 -0400

    task: added quartiles

--- Tests/test_StatisticsOps.py
+++ Tests/test_StatisticsOps.py
@@ -1,4 +1,9 @@
 from StatisticsOps import variance
+from StatisticsOps import quartiles
+
 
 def test_StatisticsOps_variance(self):
     self.assertEqual(844.0474999999999, variance.variance(self.testData))
+
+def test_StatisticOps_quartiles(self):
+    self.assertEqual([12.75, 27.5, 72.25], quartiles.quartiles(self.testData))
