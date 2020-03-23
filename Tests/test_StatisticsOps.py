e42c66548746b1d3ea2f49a8acfd5ee46ec1cb57
po47 <po47@njit.edu>
2020-03-23 16:48:21 -0400

    fix: edits

--- Tests/test_StatisticsOps.py
+++ Tests/test_StatisticsOps.py
@@ -1,9 +1,10 @@
-from StatisticsOps import variance
-from StatisticsOps import quartiles
+from StatisticsOps.quartiles import Quartiles
+from StatisticsOps.variance import Variance
 
 
 def test_StatisticsOps_variance(self):
-    self.assertEqual(844.0474999999999, variance.variance(self.testData))
+    self.assertEqual(844.0474999999999, Variance.variance(self.testData))
+
 
 def test_StatisticOps_quartiles(self):
-    self.assertEqual([12.75, 27.5, 72.25], quartiles.quartiles(self.testData))
+    self.assertEqual([12.75, 27.5, 72.25], Quartiles.quartiles(self.testData))
